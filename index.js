28ec47bcc72774a88fd695593db4099a6e82955f
Eisuke Esaki <izkyyy@gmail.com>
2022-06-22 21:54:45 -0700

    removed comments

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const port = 4242;
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   logRequest(req, res, next);
-// });
-
 app.use(session({
   secret: "a-secret",
   name: "oauth-session",
@@ -30,35 +26,4 @@ app.use(authRoutes);
 app.listen(port, () => {
   console.log("server is listening to port %d\n", port)
 });
-
-/*
-
-const { userService } = require("./lib/services");
-
-app.get("/", (req, res) => {
-  if (req.session) {
-    const user = userService.getUserById(req.session && req.session.userId);
-    if (!user)
-      res.redirect("https://accounts.google.com/o/oauth2/auth");
-    // res.redirect("http://localhost:5000/validate"); // 5000 = oauthProvider
-  }
-  else
-    return res.redirect("https://accounts.google.com/o/oauth2/auth");
-  // return res.redirect("http://localhost:5000/validate"); // 5000 = oauthProvider
-
-  res.end("if you received this message, you have a valid session");
-});
-
-app.get("/auth/google/callback", async (req, res) => {
-  // const profile = (await axios.get(`http://localhost:5000/profile?token=${req.query.token}`)).data;
-  const profile = (await axios.get(`https://oauth2.googleapis.com/token=${req.query.token}`)).data;
-  const user = userService.createUser(profile.id, profile.username, profile.password);
-
-  if (req.session)
-    req.session.userId = user.id;
-
-  res.redirect("/");
-});
-
-*/
 
